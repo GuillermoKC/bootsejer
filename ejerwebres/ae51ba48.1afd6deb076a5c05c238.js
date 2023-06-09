7cee00171f2bce06afbe8f203f2a95fd98976979
GuillermoKC <guillermokencp@gmail.com>
2023-06-09 17:30:32 +0200

    ejer tabla acabado

---
+++ ejerwebres/ae51ba48.1afd6deb076a5c05c238.js
@@ -0,0 +1,82 @@
+(window.webpackJsonp_N_E = window.webpackJsonp_N_E || []).push([
+  [4],
+  {
+    RW2Z: function (t, n, r) {
+      "use strict";
+      r.d(n, "a", function () {
+        return e;
+      }),
+        r.d(n, "b", function () {
+          return o;
+        }),
+        r.d(n, "c", function () {
+          return i;
+        });
+      var a = r("Lnxd");
+      function e(t) {
+        return Object(a.a)({
+          tag: "svg",
+          attr: { viewBox: "0 0 512 512" },
+          child: [
+            {
+              tag: "path",
+              attr: {
+                fill: "none",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "32",
+                d: "M384 240H96V136a40.12 40.12 0 0140-40h240a40.12 40.12 0 0140 40v104zM48 416V304a64.19 64.19 0 0164-64h288a64.19 64.19 0 0164 64v112",
+              },
+            },
+            {
+              tag: "path",
+              attr: {
+                fill: "none",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "32",
+                d: "M48 416v-8a24.07 24.07 0 0124-24h368a24.07 24.07 0 0124 24v8M112 240v-16a32.09 32.09 0 0132-32h80a32.09 32.09 0 0132 32v16m0 0v-16a32.09 32.09 0 0132-32h80a32.09 32.09 0 0132 32v16",
+              },
+            },
+          ],
+        })(t);
+      }
+      function o(t) {
+        return Object(a.a)({
+          tag: "svg",
+          attr: { viewBox: "0 0 512 512" },
+          child: [
+            {
+              tag: "path",
+              attr: {
+                fill: "none",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "48",
+                d: "M328 112L184 256l144 144",
+              },
+            },
+          ],
+        })(t);
+      }
+      function i(t) {
+        return Object(a.a)({
+          tag: "svg",
+          attr: { viewBox: "0 0 512 512" },
+          child: [
+            {
+              tag: "path",
+              attr: {
+                fill: "none",
+                strokeLinecap: "square",
+                strokeMiterlimit: "10",
+                strokeWidth: "48",
+                d: "M328 112L184 256l144 144",
+              },
+            },
+          ],
+        })(t);
+      }
+    },
+  },
+]);
